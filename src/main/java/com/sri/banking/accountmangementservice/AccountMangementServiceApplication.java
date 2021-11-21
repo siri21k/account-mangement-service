70c9411b446ab8e2b59f40702c750886d26ff462
Sreedevi Hanchate <siri21k@gmail.com>
2021-11-20 19:24:59 -0800

    welcome to github

--- src/main/java/com/sri/banking/accountmangementservice/AccountMangementServiceApplication.java
+++ src/main/java/com/sri/banking/accountmangementservice/AccountMangementServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccountMangementServiceApplication {
-
+//test commit
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMangementServiceApplication.class, args);
 	}
