9b78cbb0032899ba84ce8924d16c496aa5adf34a
Sreedevi Hanchate <siri21k@gmail.com>
2021-11-20 19:16:44 -0800

    initial project

---
+++ src/main/java/com/sri/banking/accountmangementservice/AccountMangementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sri.banking.accountmangementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMangementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMangementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sri/banking/accountmangementservice/AccountMangementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.banking.accountmangementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMangementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
